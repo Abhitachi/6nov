51cfbc120e513cf49c45f73afd0f81fd20eae866
Abhitachi <91671934+Abhitachi@users.noreply.github.com>
2022-01-09 22:55:47 +0530

    Add files via upload

---
+++ AdjacentChars.java
@@ -0,0 +1,36 @@
+package javatap;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class AdjacentChars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String str= scan.next();
+		scan.close();
+		Stack<Character> s =  new Stack<Character>();
+	    int n=0;
+	    String res="";
+	    while(n<str.length()) {
+	    	if(s.size()>0) {
+	    	if(s.peek()==str.charAt(n))
+	    		s.pop();
+	    	else
+	    		s.push(str.charAt(n));
+
+
+	    	}
+	    	else
+	    		s.push(str.charAt(n));
+	     n=n+1;
+	    }
+
+	    for(int i=0;i<s.size();i++) {
+	    	res+=s.get(i);
+	    }
+	    System.out.println(res);
+	}
+
+}
